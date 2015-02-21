35e1a6c1bcf7320b19cf747198f4d441c348dc3f
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2015-02-21 17:02:25 +0100

    Added comments in cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Caching the Inverse of a Matrix (Programming assignment 2)
+# Johns Hopkins University / Coursera: "R Programming"
+#
+# Solution proposed by Jean-Marc Gulliet, February 2015
+#
+# Remarks:
+#   1) I have followed the Google's Style Guide for R, 
+#   available at https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+#
+#   2) Some interresting insights about how the non-local assignment operator
+#   '<<-' works can be found at 
+#   http://www.inside-r.org/r-doc/methods/ReferenceClasses
 
 makeCacheMatrix <- function(x = matrix()) {
+    # This function creates a special "matrix" object that can cache its inverse,
+    # thus speeding up the repeated usage of this inverse matrix.
+    #
+    # Args: 
+    #   x: a square invertible R matrix.
+    #
+    # Returns:
+    #   A special "matrix" object that is a list indeed.
 
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Computes the inverse of the special "matrix" returned by makeCacheMatrix. 
+    # If the inverse has already been calculated and the matrix has not changed, 
+    # then the cachesolve function retrieves the inverse from the cache,
+    # otherwise, it computes it and caches it.
+    #
+    # Args:
+    #   x: a special "matrix" object made by makeCacheMatrix.
+    #
+    # Returns:
+    #   The inverse matrix of x.
+    
 }
