1296fbd5ae94a93f18c835f31f37ef070fa42c39
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2015-02-21 17:15:57 +0100

    Added the set function in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -11,7 +11,7 @@
 #   '<<-' works can be found at 
 #   http://www.inside-r.org/r-doc/methods/ReferenceClasses
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mat = matrix()) {
     # This function creates a special "matrix" object that can cache its inverse,
     # thus speeding up the repeated usage of this inverse matrix.
     #
@@ -20,10 +20,15 @@ makeCacheMatrix <- function(x = matrix()) {
     #
     # Returns:
     #   A special "matrix" object that is a list indeed.
-
+    
+    mat.inverse <- NULL
+    set <- function(m) {
+        mat <<- m
+        mat.inverse <<- NULL
+    }
 }
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mat, ...) {
     # Computes the inverse of the special "matrix" returned by makeCacheMatrix. 
     # If the inverse has already been calculated and the matrix has not changed, 
     # then the cachesolve function retrieves the inverse from the cache,
